4a4e45a23a12656cb401256368a7e4cf8cd9928c
Anacondaaa <bot.ijitalk@gmail.com>
2018-07-02 00:00:26 +0900

    main.go server init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	fmt.Println("server start at :8080")
+
+	http.ListenAndServe(":8080", router)
+}
